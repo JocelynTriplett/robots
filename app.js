9c24f188afa05af542f08110e15814caf8066831
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-16 16:01:53 -0400

    pulls individual user for profile page

--- app.js
+++ app.js
@@ -13,25 +13,70 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-console.log(user_profiles.users[1]);
-
 //output = Mustache.render( template, data );
 
 app.get('/', function(req, res){
    res.render('index', user_profiles)
  }
   );
- //});
+
+app.get('/:dynamic', function(req, res){
+  res.render('user', user_profiles.users[req.params.dynamic-1])
+  
+});
+//var user_id = req.params.dynamic;
+//res.render('user', user_profiles.users[0])
+// var user = user_profiles.users.filter(function( obj ) {
+// return obj.id == user_id;
+// console.log(user);
+// for (var i = 0; i < user_profiles.users.length; i++) {
+//   let user = user_profiles.users.id;
+//   console.log(user);
+// }
+// var user_id = req.params.dynamic;
+// var user = user_profiles.users.filter(function( obj ) {
+//   return obj.id === user_id;
+//   console.log(user);
+// });
+
+
 
 app.listen(3000, function(){
   console.log("the app is running on port 3000!");
 });
 
+  //
+  // console.log("request params: "+req.params.dynamic)
+  //
+  // var user = user_profiles.users.filter(function( obj ) {
+  // return obj.id == req.params.dynamic;
+
+
+
+
+//   console.log(x);
+//   var elementPos = user_profiles.users.map(function(x) {return x.id; }).indexOf(req.params);
+//   var user = user_profiles.users[elementPos];
+//
+//   res.send(`That thing you requested was ${user}`)
+//   let users = user_profiles.users;
+//   let user = "";
+//   for (var i = 0; i < users.length; i++) {
+//     if (users[i].id === req.params){
+//       user = users[i];
+//       console.log("user: "+user);
+//     }
+//   }
+//   let user = user_profiles.users[eval(req.params - 1)]
+// });
+//
+//
+//
 // app.get('/:dynamic', function(req, res){
 //   //console.log(req.params);
 //   res.send(`That thing you requested was ${req.params.dynamic}`)
 // });
-
+//
 // app.get('/template', function(req, res){
 //   res.render('index', {name: 'Lila'});
 // });
